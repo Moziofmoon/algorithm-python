5c66604ccf1c723695f5226a6e59afc1db052f84
moziofmoon@163.com <yuesf23165498GZP!>
2020-04-14 22:10:33 +0800

    1.完成70-climbing_stairs 2.完善readme

---
+++ Week_01/70_climbing_stairs.py
@@ -0,0 +1,51 @@
+# 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+#
+#  每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+#
+#  注意：给定 n 是一个正整数。
+#
+#  示例 1：
+#
+#  输入： 2
+# 输出： 2
+# 解释： 有两种方法可以爬到楼顶。
+# 1.  1 阶 + 1 阶
+# 2.  2 阶
+#
+#  示例 2：
+#
+#  输入： 3
+# 输出： 3
+# 解释： 有三种方法可以爬到楼顶。
+# 1.  1 阶 + 1 阶 + 1 阶
+# 2.  1 阶 + 2 阶
+# 3.  2 阶 + 1 阶
+#
+#  Related Topics 动态规划
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        """
+        解题思路：f(n) = f(n-1) + f(n-2),可用递归，但是时间复杂度较高
+                则利用常数替换函数
+        时间复杂度：O(n)
+        空间复杂度：O(1)
+        :param n:
+        :return:
+        """
+        first = 1
+        second = 2
+        if not n:
+            return n
+        if n == 1:
+            return first
+        if n == 2:
+            return second
+        ret = 0
+        for i in range(n - 2):
+            ret = first + second
+            first, second = second, ret
+        return ret
+# leetcode submit region end(Prohibit modification and deletion)
--- Week_01/README.md
+++ Week_01/README.md
@@ -1,4 +1,19 @@
-## 本周学习任务
+4.13-4.19 数组、链表
+
+Array 实战题目：
+
+一、https://leetcode-cn.com/problems/move-zeroes/
+
+二、https://leetcode-cn.com/problems/container-with-most-water/
+
+三、https://leetcode.com/problems/climbing-stairs/
+
+四、https://leetcode-cn.com/problems/3sum/ (高频老题）
+
+巩固练习：
+
+一、https://leetcode-cn.com/problems/plus-one/
+
 https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
 
 https://leetcode-cn.com/problems/rotate-array/
@@ -8,7 +23,3 @@ https://leetcode-cn.com/problems/merge-two-sorted-lists/
 https://leetcode-cn.com/problems/merge-sorted-array/
 
 https://leetcode-cn.com/problems/two-sum/
-
-https://leetcode-cn.com/problems/move-zeroes/
-
-https://leetcode-cn.com/problems/plus-one/
