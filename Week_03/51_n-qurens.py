525143823f1ee85d4624ff0f2acd39fe22b065b0
moziofmoon <moziofmoon@163.com>
2020-05-06 08:42:12 +0800

    1.更新51注释

--- Week_03/51_n-qurens.py
+++ Week_03/51_n-qurens.py
@@ -9,14 +9,22 @@ class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         def DFS(queens, diff_xy, sum_xy):
             rows = len(queens)
+            # 判断结束条件 当填充行数为n时结束
             if rows == n:
                 result.append(queens)
                 return
 
             for col in range(n):
+                # col in queens 代表所在列
+                # rows - col 代表捺
+                # rows + col 代表撇
                 if col not in queens and rows - col not in diff_xy and rows + col not in sum_xy:
                     DFS(queens + [col], diff_xy + [rows - col], sum_xy + [rows + col])
 
         result = []
         DFS([], [], [])
         return [["."*i + "Q" + "."*(n - i - 1) for i in sol] for sol in result]
+
+if __name__ == '__main__':
+    solution = Solution()
+    solution.solveNQueens(8)
